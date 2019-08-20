45d96b8cce507dafd846e02d7b5bcc863b59e501
Eskimo <1145400536@qq.com>
2019-08-20 11:15:04 +0800

    construct frame

---
+++ build/precache-manifest.c81d3c7938379a3c746c4f864e6a68cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc289bc9d5ae7a5326ceab65886fba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce63fa7fbbd12391dcfc",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8d100e740713f13de4b0",
+    "url": "/static/js/2.de9ab016.chunk.js"
+  },
+  {
+    "revision": "ce63fa7fbbd12391dcfc",
+    "url": "/static/js/main.e97c8e18.chunk.js"
+  },
+  {
+    "revision": "a81a17932efb57d162ab",
+    "url": "/static/js/runtime~main.52b65140.js"
+  }
+]);
